f75a09763c79b5c130f456ab4981dac924d924a2
YannGarcia <garcia.yann@gmail.com>
2017-03-20 22:04:23 +0100

    Debugging ./install.bash

--- build.bash
+++ build.bash
@@ -5,15 +5,12 @@ OLD_PWD=`pwd`
 # Use ${PATH_DEV}/g++/projectsxs/embedded/Makefile at the end of the full review
 cd ${PATH_DEV}/g++/projects/embedded/logger/objs
 make clean
-make test
 make gendoc
 cd ${PATH_DEV}/g++/projects/embedded/converter/objs
 make clean
-make test
 make gendoc
 cd ${PATH_DEV}/g++/projects/embedded/helper/objs
 make clean
-make test
 make gendoc
 cd ${OLD_PWD}
 
--- test.bash
+++ test.bash
@@ -3,6 +3,13 @@ set -evx
 
 OLD_PWD=`pwd`
 cd ${PATH_DEV}/g++/projects/embedded/logger/objs
+make test
+make run
+cd ${PATH_DEV}/g++/projects/embedded/converter/objs
+make test
+make run
+cd ${PATH_DEV}/g++/projects/embedded/helper/objs
+make test
 make run
 cd ${OLD_PWD}
 
