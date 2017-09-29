1e7638f2c51df7aac08ae35358a4d3d10ea5c647
YannGarcia <garcia.yann@gmail.com>
2017-09-29 15:26:23 +0200

    Adding code coverage support

--- travis_scripts/build.bash
+++ travis_scripts/build.bash
@@ -5,15 +5,15 @@ OLD_PWD=`pwd`
 # Use ${PATH_DEV}/g++/projectsxs/embedded/Makefile at the end of the full review
 cd ${PATH_DEV}/g++/projects/embedded/logger/objs
 make clean
-make compile_c
+make coverage
 make gendoc
 cd ${PATH_DEV}/g++/projects/embedded/converter/objs
 make clean
-make compile
+make coverage
 make gendoc
 cd ${PATH_DEV}/g++/projects/embedded/helper/objs
 make clean
-make compile
+make coverage
 make gendoc
 cd ${OLD_PWD}
 
