d32de5c53cd329b45a4d1fe10667370b903ede40
YannGarcia <garcia.yann@gmail.com>
2017-09-29 19:59:06 +0200

    Adding code coverage support

--- travis_scripts/test.bash
+++ travis_scripts/test.bash
@@ -2,18 +2,27 @@
 set -evx
 
 OLD_PWD=`pwd`
-cdxoxf ${PATH_DEV}/g++/projects/embedded/logger/objs
-make test_c
-make run_c
+cd ${PATH_DEV}/g++/projects/embedded/logger/objs
+if [ ${TRAVIS_CONTEXT} == "NoCoveralls" ]
+then
+   make test_d
+   make run_d
+elif [ ${TRAVIS_CONTEXT} == "WithCoveralls" ]
+   make test_c
+   make run_c
+   if [ -f ./logger.coverage.info ]
+   then
+       coveralls-lcov --verbose ./logger.coverage.info
+   fi
+else
+   make test
+   make run_v    
+fi
 if [ ! -d ../docs/htm ]
 then
     exit -1
 fi
 ls -ltr
-if [ -f ./logger.coverage.info ]
-then
-    coveralls-lcov --verbose ./logger.coverage.info
-fi
 cd ${PATH_DEV}/g++/projects/embedded/converter/objs
 make test
 make run
