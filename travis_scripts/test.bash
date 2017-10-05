ecaa0da6943aa383a55879c6321349d977aec66f
YannGarcia <garcia.yann@gmail.com>
2017-10-05 13:28:53 +0000

    Prepare Github Pages hosting & enhance Travis scripts

--- travis_scripts/test.bash
+++ travis_scripts/test.bash
@@ -3,14 +3,31 @@
 set -evx
 
 function check_docs {
-    if [ ! -d ../docs/html ]
+    if [ -d ../docs/html ]
     then
-       return "-1"
-    elif [ ! -d ../docs/latex ]
-    then
+        if [ ! -f ../docs/html/index.html ]
+        then
+            return "-1"
+        fi
+    else
         return "-1"
-    elif [ ! -d ../docs/man ]
+    fi
+    if [ -d ../docs/latex ]
     then
+        if [ ! -f ../docs/latex/Makefile ]
+        then
+            return "-1"
+        fi
+    else
+        return "-1"
+    fi
+    if [ -d ../docs/man ]
+    then
+        if [ ! -f ../docs/man/mainpage.md.3 ]
+        then
+            return "-1"
+        fi
+    else
         return "-1"
     fi
 
@@ -58,14 +75,16 @@ function check_gtest_verdicts {
     then
         return "-1"
     fi
+    nb_failure="-1"
     nb_failures=cat ../objs/gtestresults.xml | grep "<testsuites tests=" | cut -d' ' -f3 | cut -d'=' -f2
 
     return $nb_failures
 }
 
 OLD_PWD=`pwd`
-cd ${PATH_DEV}/g++/projects/embedded/logger/objs
+
 check_lib "logger"
+cd ${PATH_DEV}/g++/projects/embedded/${check_lib}/objs
 if [ "$?" != "0" ]
 then
     echo "Checking lib failed"
@@ -78,6 +97,39 @@ then
     exit -3
 fi
 declare -a include_files=("logger_factory.h" "logger.h" "logger_levels.h" "logger_time_formats.h")
+check_includes "${include_files[@]}"
+if [ "$?" != "0" ]
+then
+    echo "Checking includes failed"
+    exit -4
+fi
+check_docs
+if [ "$?" != "0" ]
+then
+    echo "Checking docs failed"
+    exit -1
+fi
+check_gtest_verdicts
+if [ "$?" != "0" ]
+then
+    echo "GoogleTest found FAIL verdicts"
+    exit -1
+fi
+
+check_lib "helpers"
+cd ${PATH_DEV}/g++/projects/embedded/${check_lib}/objs
+if [ "$?" != "0" ]
+then
+    echo "Checking lib failed"
+    exit -2
+fi
+check_test "testlib"
+if [ "$?" != "0" ]
+then
+    echo "Checking test failed"
+    exit -3
+fi
+declare -a include_files=("date_time.h" "helper.h" "ibstream.h" "keyboard.h" "obstream.t.h" "get_opt.h" "helper.t.h" "ibstream.t.h" "obstream.h" "runnable.h")
 check_includes "${include_files[@]}"
 if [ "$?" != "0" ]
 then
