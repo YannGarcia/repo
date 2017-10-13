b3aaed2a4d20cdddeb80b6d2856ebe824a4b55af
YannGarcia <garcia.yann@gmail.com>
2017-10-13 09:04:42 +0000

    Bug fixed timezone setting required for test units

--- dev/g++/projects/embedded/converter/test/testlib.cpp
+++ dev/g++/projects/embedded/converter/test/testlib.cpp
@@ -206,7 +206,7 @@ TEST(converter_test_suite, time_to_string_1) {
   time_t current_time = 1489755780;
   std::string str = converter::get_instance().time_to_string(current_time);
   cout << "test_time_to_string_1: " << str << "\r" << endl;
-  std::string result("Fri, 17 Mar 2017 14:03:00 +0100");
+  std::string result("Fri, 17 Mar 2017 13:03:00 +0000");
   cout << "expected test_time_to_string_1: " << result << "\r" << endl;
   cout << "test_time_to_string_1: " << str.compare(result) << "\r" << endl;
   ASSERT_TRUE(str.compare(result) == 0); // http://www.unixtimestamp.com/
