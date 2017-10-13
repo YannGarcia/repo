dc57de403d5e91a1236fe68c06d36d0fec13a3af
YannGarcia <garcia.yann@gmail.com>
2017-10-13 12:03:12 +0000

    Bug fixed in Travis CI scripts

--- travis_scripts/deploy.bash
+++ travis_scripts/deploy.bash
@@ -2,20 +2,27 @@
 set -e # Exit with non 0 if any command fails
 set -vx
 
-OLD_PWD=`pwd`
-cd ${PATH_DEV}/g++/projects/embedded
-make pushO2
-tree ${HOME}/docs
-make clean
+if [ ${TRAVIS_CONTEXT} == "NoCoveralls" ]
+then
+    OLD_PWD=`pwd`
+    cd ${PATH_DEV}/g++/projects/embedded
+    make pushO2
+    
+    cd ${HOME_TMP}
+    mkdir docs
+    cd ./docs
+    git clone https://github.com/YannGarcia/repo.git .
+    git config user.name "YannGarcia"
+    git config user.email "garcia.yann@gmail.com"
+    git checkout origin/gh-pages -b gh-pages
+    mv ${HOME}/docs/* .
+    git add .
+    git commit -m "New generated HTML doc"
+    git push origin gh-pages
+    cd ${HOME_TMP}
+    rm -fr ./docs
 
+    cd ${OLD_PWD}
+fi
 
-#cd ${HOME}/docs
-#git config user.name "YannGarcia"
-#git config user.email "garcia.yann@gmail.com"
-#git add .
-#git commit -a -m""
-#git push
-
-
-cd ${OLD_PWD}
 exit 0
