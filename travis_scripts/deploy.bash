a2954e88d24069d6e86bfa35817493d7a80f70c4
YannGarcia <garcia.yann@gmail.com>
2017-10-13 12:20:32 +0000

    Bug fixed in Travis CI scripts

--- travis_scripts/deploy.bash
+++ travis_scripts/deploy.bash
@@ -8,19 +8,19 @@ then
     cd ${PATH_DEV}/g++/projects/embedded
     make pushO2
     
-    cd ${HOME_TMP}
-    mkdir docs
-    cd ./docs
-    git clone https://github.com/YannGarcia/repo.git .
-    git config user.name "YannGarcia"
-    git config user.email "garcia.yann@gmail.com"
-    git checkout origin/gh-pages -b gh-pages
-    mv ${HOME}/docs/* .
-    git add .
-    git commit -m "New generated HTML doc"
-    git push origin gh-pages
-    cd ${HOME_TMP}
-    rm -fr ./docs
+#    cd ${HOME_TMP}
+#    mkdir docs
+#    cd ./docs
+#    git clone https://github.com/YannGarcia/repo.git .
+#    git config user.name "YannGarcia"
+#    git config user.email "garcia.yann@gmail.com"
+#    git checkout origin/gh-pages -b gh-pages
+#    mv ${HOME}/docs/* .
+#    git add .
+#    git commit -m "New generated HTML doc"
+#    git push origin gh-pages
+#    cd ${HOME_TMP}
+#    rm -fr ./docs
 
     cd ${OLD_PWD}
 fi
