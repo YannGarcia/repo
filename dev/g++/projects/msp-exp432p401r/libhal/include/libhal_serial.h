36210f86e5a310ddbd60cdc4d672192b7acb3dc8
Yann Garcia <garcia.yann@gmail.com>
2017-10-21 16:39:45 +0200

    Merge libhal_serial.h changes for TI MSP432

--- dev/g++/projects/msp-exp432p401r/libhal/include/libhal_serial.h
+++ dev/g++/projects/msp-exp432p401r/libhal/include/libhal_serial.h
@@ -19,41 +19,49 @@ extern "C" {
 #endif
 
   /**
-   * @fn int32_t serial_open(const int8_t *p_device, const int32_t p_baud_rate)
+   * @fn int32_t serial_open(const char *p_device, const uint32_t p_baud_rate)
    * @brief Open and initialise the serial port (8, N, 1)
    * @param[in] p_device    The serial device name (e.g. /dev/tty0)
    * @param[in] p_baud_rate The baud value
    * @return The serial file descriptor on success, -1 otherwise
    */
-  extern int32_t serial_open(const int8_t *p_device, const int32_t p_baud_rate);
+  extern int32_t serial_open(const char *p_device, const uint32_t p_baud_rate);
   /**
-   * @fn void serial_close(const int32_t p_fd)
+   * @fn int32_t serial_open_id(const uint8_t p_device, const uint32_t p_baud_rate)
+   * @brief Open and initialise the serial port (8, N, 1)
+   * @param[in] p_device    The serial device identifier (e.g. 0 stands for /dev/tty0)
+   * @param[in] p_baud_rate The baud value
+   * @return The serial file descriptor on success, -1 otherwise
+   */
+  extern int32_t serial_open_id(const uint8_t p_device, const uint32_t p_baud_rate);
+  /**
+   * @fn void serial_close(const uint32_t p_fd)
    * @brief Close the serial port
    * @param[in] p_fd    The serial file descriptor
    */
-  extern void serial_close(const int32_t p_fd);
+  extern void serial_close(const uint32_t p_fd);
   /**
-   * @fn void serial_flush(const int32_t p_fd)
+   * @fn void serial_flush(const uint32_t p_fd)
    * @brief Flush the serial buffers (both TX & RX)
    * @param[in] p_fd    The serial file descriptor
    */
-  extern void serial_flush(const int32_t p_fd);
+  extern void serial_flush(const uint32_t p_fd);
   /**
-   * @fn void serial_put_char(const int32_t p_fd, const uint8_t p_char)
+   * @fn void serial_put_char(const uint32_t p_fd, const char p_char)
    * @brief Send a character
    * @param[in] p_fd    The serial file descriptor
    * @param[in] p_char  The character to send
    */
-  extern void serial_put_char(const int32_t p_fd, const uint8_t p_char);
+  extern void serial_put_char(const uint32_t p_fd, const char p_char);
   /**
-   * @fn void serial_put_string(const int32_t p_fd, const int8_t *p_string)
+   * @fn void serial_put_string(const uint32_t p_fd, const int8_t *p_string)
    * @brief Send a NULL terminated string
    * @param[in] p_fd        The serial file descriptor
    * @param[in] p_string    The string to send
    */
-  extern void serial_put_string(const int32_t p_fd, const int8_t *p_string);
+  extern void serial_put_string(const uint32_t p_fd, const char *p_string);
   /**
-   * @fn void serial_printf(const int32_t p_fd, const int8_t *p_message, ...)
+   * @fn void serial_printf(const uint32_t p_fd, const char *p_message, ...)
    * @brief Send a printf-like formated message
    * @param[in] p_fd        The serial file descriptor
    * @param[in] p_message   The printf-like formated message
@@ -61,23 +69,23 @@ extern "C" {
    *                            %c, %d or %i, %u, %x or %X or %%
    * @remark If the output indicate ERROR, one of the printf arguments is not supported (e.g. %f)
    */
-  extern void serial_printf(const int32_t p_fd, const int8_t *p_message, ...);
+  extern void serial_printf(const uint32_t p_fd, const char *p_message, ...);
   /**
-   * @fn int32_t serial_data_available(const int32_t p_fd)
+   * @fn int32_t serial_data_available(const uint32_t p_fd)
    * @brief Retrieve the number of bytes avalable for read
    * @param[in] p_fd    The serial file descriptor
    * @return The number of bytes avalable for read on success, -1 otherwise
    */
-  extern int32_t serial_data_available(const int32_t p_fd);
+  extern int32_t serial_data_available(const uint32_t p_fd);
   /**
-   * @fn int32_t serial_get_char(const int32_t p_fd)
+   * @fn int32_t serial_get_char(const uint32_t p_fd)
    * @brief Retrieve a single character
    * @param[in] p_fd    The serial file descriptor
    * @return The read character on success, -1 otherwise
    * @remark NULL is a valid character
    * @remark This function will time-out after 10 seconds (\see serial_open)
    */
-  extern int32_t serial_get_char(const int32_t p_fd);
+  extern int32_t serial_get_char(const uint32_t p_fd);
   /**
    * @fn void ftoa(float p_value, int8_t *p_buffer, uint8_t p_resolution)
    * @brief Convert a floating point number to a null-terminated string
