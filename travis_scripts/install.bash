ae8ec3656da69e14f75821dfc3127e0b2f2284e8
YannGarcia <garcia.yann@gmail.com>
2017-10-25 08:58:27 +0000

    Debug Travis bash files

--- travis_scripts/install.bash
+++ travis_scripts/install.bash
@@ -111,7 +111,7 @@ elif [ "${TRAVIS_CONTEXT}" == "BaremetalHW" ]
 then
     # Install ARM cross compile for bare metal hardware
     df -k
-    wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
+    wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2 > /dev/null 3>&1
     bzip2 -d ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
     tar xvf ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar > /dev/null 3>&1
     #rm ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
