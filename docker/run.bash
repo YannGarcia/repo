3d3280aeecd66dac49fb5919b12c9e2c8ad2c268
YannGarcia <garcia.yann@gmail.com>
2017-11-03 14:40:24 +0000

    [Skip CI] Enhance script

--- docker/run.bash
+++ docker/run.bash
@@ -5,7 +5,7 @@ set -vx
 if [ -f ./Dockerfile.$1 ]
 then
 #    docker run -it --name my$1 --rm --volume /f:/media/sf_F_DRIVE $1 /bin/bash
-    docker run -it -h docker-$1 --name my$1 --rm $1 /bin/bash
+    docker run -it --name my$1 --rm $1 /bin/bash
 fi
 
 exit 0
