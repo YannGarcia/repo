82f32708e463f29acc6ff20cc985c398caba52da
Garcia <garcia.yann@gmail.com>
2017-11-11 18:10:16 +0100

    Update install.bash

--- travis_scripts/install.bash
+++ travis_scripts/install.bash
@@ -110,13 +110,13 @@ then
 elif [ "${TRAVIS_CONTEXT}" == "BaremetalHW" ]
 then
     # Install ARM cross compile for bare metal hardware
-#    wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
-#    bzip2 -d ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
-#    tar xvf ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar
+    wget https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q3-update/+download/gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
+    bzip2 -d ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar.bz2
+    tar xvf ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar
 #    rm ./gcc-arm-none-eabi-5_4-2016q3-20160926-linux.tar
 
-#    ./gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-gcc --version
-#    ./gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-g++ --version
+    ./gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-gcc --version
+    ./gcc-arm-none-eabi-5_4-2016q3/bin/arm-none-eabi-g++ --version
 
     export SELECTED_HW="ti_msp_exp432p401r"
     MBED_CROSS_COMPILER_VERSION=5.4.1
